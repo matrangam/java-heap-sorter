76c74603e956868466c706c84dc641a8884b351d
Mike Matranga <mike@dmgctrl.com>
2013-03-22 18:33:35 -0400

    Added basic testing.

--- src/com/dimsum/livewallpaperclock/MainActivity.java
+++ src/com/dimsum/livewallpaperclock/MainActivity.java
@@ -2,6 +2,7 @@ package com.dimsum.livewallpaperclock;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
@@ -28,7 +29,25 @@ public class MainActivity extends Activity {
 		
 		TextClock year = (TextClock) findViewById(R.id.year);
 		year.setFormat12Hour("yyyy");
+
+		PriorityQueue priorityQueue = new PriorityQueue(13);
+		priorityQueue.insert(10);
+		priorityQueue.insert(100);
+		priorityQueue.insert(9);
+		priorityQueue.insert(8);
+		priorityQueue.insert(1);
+		priorityQueue.insert(2);
+		priorityQueue.insert(19);
+		priorityQueue.insert(32);
+		priorityQueue.insert(22);
+		priorityQueue.insert(23);
+		priorityQueue.insert(24);
+		priorityQueue.insert(62);
+		priorityQueue.insert(3);
 		
+		for (int i = 0; i < priorityQueue.getArray().length; i++) {
+			Log.v("ME", "Index: " + i + " = " + priorityQueue.getArray()[i]);
+		}
 	}
 
 	@Override
---
+++ src/com/dimsum/livewallpaperclock/PriorityQueue.java
@@ -0,0 +1,50 @@
+package com.dimsum.livewallpaperclock;
+
+public class PriorityQueue {
+	
+	private final int MAX_SIZE;
+	private int[ ] array;
+    private static final int ROOT = 0;
+    private int lastIndex = ROOT;
+    
+	public PriorityQueue(int size) {
+		MAX_SIZE= size;
+		setArray(new int[MAX_SIZE]);
+		java.util.Arrays.fill(getArray(), -1);
+	}
+	
+	public void insert(int element) {
+		if (lastIndex == MAX_SIZE) {
+			return;
+		}
+		int elementIndex = lastIndex++;
+		getArray()[elementIndex] = element;
+		
+		while (elementIndex != 0 && getArray()[elementIndex] < getArray()[parent(elementIndex)]) {
+			swap(elementIndex, parent(elementIndex));
+			elementIndex = parent(elementIndex);
+		}
+	}
+	
+	private int parent(int x) {
+		return ((x + (x & 1)) / 2) - 1;
+	}
+
+	private int rightChildOf(int x) {
+		return (x + 1) * 2;
+	}
+	
+	private void swap(int a, int b) {
+		int placeholder = getArray()[a];
+		getArray()[a] = getArray()[b];
+		getArray()[b] = placeholder;		
+	}
+
+	public int[ ] getArray() {
+		return array;
+	}
+
+	private void setArray(int[ ] array) {
+		this.array = array;
+	}
+}
